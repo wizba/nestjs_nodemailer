be64b70251eead037a69462d9a53eba9786e6b4d
william <williamrabs@gmail.com>
2020-07-30 22:47:39 +0200

    now fixed 2

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,8 +9,8 @@ export class AppService {
     this.transporter = nodemailer.createTransport({
       service:'gmail',// you can also use other services like outlook
       auth:{
-        user:process.env.EMAIL,//replace with your email
-        pass:process.env.PASSWORD//replace with your password
+        user:'williamrabs@gmail.com',//replace with your email
+        pass:'mw@53820'//replace with your password
       }
     });
   }
